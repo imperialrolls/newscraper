dd8660627a038401fa03ed50792dbfb5f82587d8
Christopher Rolls <imperialrolls@gmail.com>
2018-03-21 16:07:00 -0700

    heroku disaster

--- server.js
+++ server.js
@@ -27,11 +27,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Use express.static to serve the public folder as a static directory
 app.use(express.static('public'));
 
-// By default mongoose uses callbacks for async queries, we're setting it to use promises (.then syntax) instead
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/mongoHeadlines';
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
 // Connect to the Mongo DB
 mongoose.Promise = Promise;
-mongoose.connect('mongodb://localhost/newScraper', {
-  useMongoClient: true,
+mongoose.connect(MONGODB_URI, {
+  useMongoClient: true
 });
 
 // Routes
