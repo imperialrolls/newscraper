13081f8ea1757de2ea4eac33488fb3610d1b8f77
Christopher Rolls <imperialrolls@gmail.com>
2018-03-22 15:33:08 -0700

    Handlebars view engine setup

--- server.js
+++ server.js
@@ -4,24 +4,29 @@ var bodyParser = require('body-parser');
 var logger = require('morgan');
 var mongoose = require('mongoose');
 
-// Our scraping tools
+//Our scraping tools
 var axios = require('axios');
 var cheerio = require('cheerio');
 
-// Require all models
+//Require all models
 var db = require('./models');
 
 var port = process.env.PORT || 3000;
 
-// Initialize Express
+//Initialize Express
 var app = express();
 
-// Configure middleware
+//Handlebars view engine setup
+const exphbs = require('express-handlebars');
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 
-// Use morgan logger for logging requests
+//Configure middleware
+
+//Use morgan logger for logging requests
 app.use(logger('dev'));
 
-// Require and use routes modules
+//Require and use routes modules
 const index = require('./routes/index');
 const scrape = require('./routes/scrape');
 const article = require('./routes/articles');
@@ -32,14 +37,14 @@ app.use('/scrape', scrape);
 app.use('/articles', article);
 app.use('/notes', notes);
 
-// Use body-parser for handling form submissions
+//Use body-parser for handling form submissions
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Use express.static to serve the public folder as a static directory
+//Use express.static to serve the public folder as a static directory
 app.use(express.static('public'));
 
-// Set mongoose database connection
+//Set mongoose database connection
 const databaseUri = 'mongodb://localhost/newScraper';
 
 if (process.env.MONGODB_URI) {
@@ -48,7 +53,7 @@ if (process.env.MONGODB_URI) {
   mongoose.connect(databaseUri);
 }
 
-// Start the server
+//Start the server
 app.listen(port, function () {
   console.log('App running on port ' + port + '!');
 });
