aaafee3f8d57f43d2473b893cd5db4db3bf8d464
Christopher Rolls <imperialrolls@gmail.com>
2018-03-21 17:11:30 -0700

    port update in server.js

--- server.js
+++ server.js
@@ -111,10 +111,8 @@ app.post('/articles/:id', function (req, res) {
   // Create a new note and pass the req.body to the entry
   db.Note.create(req.body)
     .then(function (dbNote) {
-      // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
-      // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
-      // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
-      return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+      return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id },
+        { new: true });
     })
     .then(function (dbArticle) {
       // If we were able to successfully update an Article, send it back to the client
@@ -127,6 +125,6 @@ app.post('/articles/:id', function (req, res) {
 });
 
 // Start the server
-app.listen(PORT, function () {
-  console.log('App running on port ' + PORT + '!');
+app.listen(port, function () {
+  console.log('App running on port ' + port + '!');
 });
